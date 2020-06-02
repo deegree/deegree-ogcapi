c7b16667eed947a86f6501ceac2c79438b161fbf
Lyn Elisa Goltz <goltz@lat-lon.de>
2020-06-02 08:35:05 +0200

    OAF-121 - fixed warning

--- deegree-ogcapi-features/src/main/java/org/deegree/services/oaf/resource/html/Webjars.java
+++ deegree-ogcapi-features/src/main/java/org/deegree/services/oaf/resource/html/Webjars.java
@@ -6,7 +6,9 @@ import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -21,6 +23,7 @@ public class Webjars {
     @Context
     ServletContext servletContext;
 
+    @Produces({"text/javascript", "text/css"})
     @Operation(hidden = true)
     @GET
     public InputStream getFile( @PathParam("path") String path ) {
