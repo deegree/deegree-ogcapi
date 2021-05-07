7133b0909e14f2d335fb65e04fbf68cfd46e1150
Lyn Elisa Goltz <goltz@lat-lon.de>
2021-05-07 09:20:56 +0200

    #261 (OAF-277) - hide resource /appschemas/{path}

--- deegree-ogcapi-features/src/main/java/org/deegree/services/oaf/resource/AppschemaFile.java
+++ deegree-ogcapi-features/src/main/java/org/deegree/services/oaf/resource/AppschemaFile.java
@@ -23,21 +23,8 @@ package org.deegree.services.oaf.resource;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.deegree.feature.persistence.FeatureStore;
-import org.deegree.feature.types.AppSchema;
-import org.deegree.feature.types.FeatureType;
-import org.deegree.gml.schema.GMLSchemaInfoSet;
 import org.deegree.services.oaf.exceptions.UnknownAppschema;
-import org.deegree.services.oaf.exceptions.UnknownCollectionId;
-import org.deegree.services.oaf.exceptions.UnknownDatasetId;
-import org.deegree.services.oaf.link.LinkBuilder;
-import org.deegree.services.oaf.schema.ExistingSchemaResponse;
-import org.deegree.services.oaf.schema.GeneratedSchemaResponse;
-import org.deegree.services.oaf.schema.SchemaResponse;
 import org.deegree.services.oaf.workspace.DeegreeWorkspaceInitializer;
-import org.deegree.services.oaf.workspace.configuration.FeatureTypeMetadata;
-import org.deegree.services.oaf.workspace.configuration.OafDatasetConfiguration;
-import org.deegree.services.oaf.workspace.configuration.OafDatasets;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -45,23 +32,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import javax.xml.namespace.QName;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 
-import static java.util.stream.Collectors.toMap;
 import static javax.ws.rs.core.MediaType.APPLICATION_XML;
-import static org.deegree.gml.GMLVersion.GML_32;
 
 /**
  * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz </a>
@@ -74,7 +50,7 @@ public class AppschemaFile {
 
     @GET
     @Produces({ APPLICATION_XML })
-    @Operation(operationId = "appschema",
+    @Operation(hidden = true, operationId = "appschema",
                     summary = "retrieve application schema of collection {collectionId}",
                     description = "Retrieves the application schema of the collection with the id {collectionId}")
     @Tag(name = "Schema")
